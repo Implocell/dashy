fe8ecf24901754e3343aed9ccd6628234724ecb0
Ola Lium <olalium@gmail.com>
2023-03-05 22:07:46 +0100

    feat: add submit button and onSubmit loading logic

--- frontend/src/components/MemeInput/MemeInput.tsx
+++ frontend/src/components/MemeInput/MemeInput.tsx
@@ -1,15 +1,51 @@
-import { Box, TextField, Typography } from '@suid/material';
+import { Send } from '@suid/icons-material';
+import { Box, CircularProgress, IconButton, TextField, Typography } from '@suid/material';
+import { createSignal } from 'solid-js';
 import { useMemeExampleText } from './hooks';
+
+interface MemeInputEndAdornmentProps {
+	isLoading: boolean;
+	onSubmit: () => void;
+}
+
+const MemeInputEndAdornment = (props: MemeInputEndAdornmentProps) => {
+	return (
+		<IconButton size="small" onClick={() => props.onSubmit()} disabled={props.isLoading}>
+			{props.isLoading ? <CircularProgress size={24} /> : <Send color="primary" fontSize="small" />}
+		</IconButton>
+	);
+};
 
 export const MemeInput = () => {
 	const { memeText } = useMemeExampleText();
+	const [isLoading, setIsLoading] = createSignal(false);
+
+	const onSubmit = () => {
+		setIsLoading(true);
+	};
+
+	const onKeyDown = (event: KeyboardEvent) => {
+		if (event.key === 'Enter') {
+			onSubmit();
+		}
+	};
 
 	return (
 		<Box pb={4} sx={{ display: 'flex' }} flexDirection="column">
-			<TextField size="small" placeholder="insert meme here!" />
+			<Box>
+				<TextField
+					size="small"
+					placeholder="Insert meme here!"
+					disabled={isLoading()}
+					InputProps={{
+						onKeyDown: onKeyDown,
+						endAdornment: <MemeInputEndAdornment isLoading={isLoading()} onSubmit={onSubmit} />,
+					}}
+				/>
+			</Box>
 			<Box height={16}>
 				<Typography variant="caption" fontStyle="italic" color="GrayText">
-					{memeText}
+					{isLoading() ? 'Creating meme...' : memeText}
 				</Typography>
 			</Box>
 		</Box>
